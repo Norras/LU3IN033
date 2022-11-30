5933bd3517d44d0cd90a7f39fbdc51b3eb28caad
Norras <arkusreturn@gmail.com>
2022-12-01 00:16:03 +0100

    nouveaux ajouts

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ from input import input #import the input function from input.py
 
 
 Ethernet=tuple[str,str,str]
-Ip=tuple[int,int,int,str,str,str,int,int,str,str,str,str]
+Ip=tuple[str,str,str,str,str,str,str,str,str,str,str]
 
 # Define a empty value in tuple : put None
 
@@ -19,29 +19,40 @@ def extract_ip_header(packet)->Ip:
     #determine the ip header length
     #ip header length is in the 4th byte of the ip header
 
-    total_length = int(''.join(packet[16:18]),16)
-    ip_header = packet[14:14+total_length]
-    version = int(ip_header[0][0],16)
-    ihl = int(ip_header[0][1],16)*4 # ip header length in bytes
-    identification = int(''.join(ip_header[4:6]),16)
-    flags = int(ip_header[6][0],16)
-    fragment_offset = int(''.join(ip_header[6:8]),16)
-    ttl = int(ip_header[8],16) # time to live
-    protocol = int(ip_header[9],16)
+    total_length_int = int(''.join(packet[16:18]),16)
+    total_length=''.join(packet[16:18])
+    ip_header = packet[14:14+total_length_int]
+    version = ip_header[0][0]
+    ihl_int = int(ip_header[0][1],16)*4 # ip header length in bytes
+    ihl=ip_header[0][1]
+    identification = ''.join(ip_header[4:6])
+    fragment_offset_and_flags = ''.join(ip_header[6:8])
+    ttl = ip_header[8] # time to live
+    protocol = ip_header[9]
     header_checksum = ''.join(ip_header[10:12])
-    source_address = int(''.join(ip_header[12:16]),16)
-    destination_address = int(''.join(ip_header[16:20]),16)
-    options:str = ''.join(ip_header[20:ihl])
-    return (version,ihl,total_length,identification,flags,fragment_offset,ttl,protocol,header_checksum,source_address,destination_address,options)
+    source_address = ''.join(ip_header[12:16])
+    destination_address = ''.join(ip_header[16:20])
+    options:str = ''.join(ip_header[20:ihl_int])
+    return (version,ihl,total_length,identification,fragment_offset_and_flags,ttl,protocol,header_checksum,source_address,destination_address,options)
 
     
 
 def value_to_ip(value:int)->str:
     return str(value>>24)+"."+str((value>>16)&0xFF)+"."+str((value>>8)&0xFF)+"."+str(value&0xFF)
 
-
-
+# dissociate flags from fragment offset
+# warning : returned values are in base 2
+def extract_flags(fragment_offset_and_flags:str)->tuple[str,str]:
+    #format( "016b") to have 16 bits in binary
+    print(int(fragment_offset_and_flags,16))
+    bits = str(format(int(fragment_offset_and_flags,16),"016b"))
+    flags = bits[0:3]
+    fragment_offset = bits[3:]
+    return (flags,fragment_offset)
+    
 
 print("-------------------")
 print(extract_ethernet_header(input()[0]))
 print(extract_ip_header(input()[0]))
+extract_flags(extract_ip_header(input()[0])[4])
+print(extract_flags(extract_ip_header(input()[0])[4]))
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-, ethernet_header[12:14]
