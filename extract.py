9b1a72822074a40b2e54f9aa59db0683408174ae
Norras <arkusreturn@gmail.com>
2022-12-01 21:05:52 +0100

    tcp extraction faite

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ from input import input #import the input function from input.py
 
 Ethernet=tuple[str,str,str]
 Ip=tuple[str,str,str,str,str,str,str,str,str,str,str]
+Tcp=tuple[str,str,str,str,str,str,str,str,str]
 
 # Define a empty value in tuple : put None
 
@@ -50,11 +51,54 @@ def extract_flags(fragment_offset_and_flags:str)->tuple[str,str]:
     fragment_offset = bits[3:]
     return (flags,fragment_offset)
     
+def check_if_ip(packet)->bool:
+    return packet[12:14]==['08','00']
 
+def check_if_tcp(packet)->bool:
+    return packet[23]=='06'
+
+def extract_tcp(packet)->Tcp:
+    # check ip header length
+    ihl_int = int(packet[14][1],16)*4
+    # check tcp header length
+    tcp_header_length_byte = str(format(int(packet[14+ihl_int+12],16),"08b"))
+    tcp_header_length = int(tcp_header_length_byte[0:4],2)*4
+
+    print(ihl_int)
+    print(tcp_header_length)    
+    print(14+ihl_int)
+    tcp_header=packet[14+ihl_int:14+ihl_int+tcp_header_length]
+    print(tcp_header)
+    source_port = ''.join(tcp_header[0:2])
+    destination_port = ''.join(tcp_header[2:4])
+    sequence_number = ''.join(tcp_header[4:8])
+    acknowledgement_number = ''.join(tcp_header[8:12])
+    flags_and_stuff=''.join(tcp_header[12:14])
+    flags=extract_tcp_flags(flags_and_stuff)
+    window_size = ''.join(tcp_header[14:16])
+    checksum = ''.join(tcp_header[16:18])
+    urgent_pointer = ''.join(tcp_header[18:20])
+    options = ''.join(tcp_header[20:])
+
+    return (source_port,destination_port,sequence_number,acknowledgement_number,tcp_header_length_byte,flags,window_size,checksum,urgent_pointer,options)
+
+
+def extract_tcp_flags(flags_and_stuff:str)->tuple[str,str,str,str,str,str,str,str]:
+    bits = str(format(int(flags_and_stuff,16),"016b"))
+    print(bits)
+    reserved=bits[4:10]
+    urg = bits[10]
+    ack = bits[11]
+    psh = bits[12]
+    rst = bits[13]
+    syn = bits[14]
+    fin = bits[15]
+    return (reserved,urg,ack,psh,rst,syn,fin)
 
 print("-------------------")
 print(extract_ethernet_header(input()[0]))
 print(extract_ip_header(input()[0]))
 extract_flags(extract_ip_header(input()[0])[4])
+
 print(extract_flags(extract_ip_header(input()[0])[4]))
-print(~10)
+print(extract_tcp(input()[0]))
