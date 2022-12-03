813591b5ac637ee73701ac1c142a72df7c3be070
Norras <arkusreturn@gmail.com>
2022-12-03 18:13:57 +0100

    interface modifié

--- interface.py
+++ interface.py
@@ -5,10 +5,14 @@ from tkinter import messagebox
 import os
 import ctypes   
 
-window=Tk()
-window.geometry("720x440")
-window.title("Visualisateur de trames")
 
+# Characters : ⟵⎯⟶
+window=Tk()
+window.geometry("1200x600")
+window.title("Visualisateur de trames")
+window.columnconfigure(0,weight=1)
+window.rowconfigure(0,weight=1)
+window.rowconfigure(1,weight=1)
 
 # create a menu that display frames in a listbox and a scrollbar
 menubar = Menu(window)
@@ -27,7 +31,7 @@ def save_file():
 def about():
     messagebox.showinfo("About", "This is a simple tool to visualize network frames")
 
-filemenu = Menu(menubar, tearoff=0)
+filemenu = Menu(menubar, tearoff=0,font=("Helvetica", 12))
 filemenu.add_command(label="Open", command=lambda: open_file())
 filemenu.add_command(label="Save as", command=lambda: save_file())
 filemenu.add_separator()
@@ -42,7 +46,40 @@ helpmenu.add_command(label="About", command=lambda: about())
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 
+#create a frame to display the listbox and the scrollbar
+frame=Frame(window)
+frame.grid(row=0,column=0,sticky="nsew")
+frame.rowconfigure(0,weight=1)
+frame.columnconfigure(0,weight=1)
 
+# create a listbox to display the frames
+listbox = Listbox(frame, width=100, height=20,borderwidth=0,highlightthickness=0,font=("Helvetica", 18))
+listbox.grid(row=0, column=0,padx=5,pady=5,sticky="nsew")
+
+# create a scrollbar to scroll the listbox
+scrollbar = Scrollbar(frame, orient="vertical")
+scrollbar.grid(row=0, column=1,padx=0,pady=0,sticky="ns")
+scrollbar.config(command=listbox.yview)
+listbox.config(yscrollcommand=scrollbar.set)
+
+#create a horizontal scrollbar to scroll the listbox
+scrollbar2 = Scrollbar(frame, orient="horizontal")
+scrollbar2.grid(row=1, column=0,padx=0,pady=0,sticky="ew")
+scrollbar2.config(command=listbox.xview)
+listbox.config(xscrollcommand=scrollbar2.set)
+
+
+frame2=Frame(window,width=100,height=70,borderwidth=0,highlightthickness=0,relief=SUNKEN)
+frame2.grid(row=1,column=0,sticky="nsew")
+frame2.rowconfigure(0,weight=1)
+frame2.columnconfigure(0,weight=1)
+frame2.pack_propagate(0)
+
+# create an element into the listbox
+# element label in the center
+listbox.insert(END, "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⟶")
+
+listbox.itemconfig(0,{'bg':'#e63030'})
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 photo=PhotoImage(file="icon.png")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,2 @@
-
-# # create a button to open a file
-# button1 = Button(frame2, text="Open", command=lambda: open_file())
-# button1.pack(side=LEFT) 
-
-
-# # create a button to save the file
-# button2 = Button(frame2, text="Save as", command=lambda: save_file())
-# button2.pack(side=LEFT)
-
-# # create a button to quit the program
-# button3 = Button(frame2, text="Exit", command=window.quit)
-# button3.pack(side=LEFT)
-
-# # create a button to display the about window
-# button4 = Button(frame2, text="About", command=lambda: about())
-# button4.pack(side=LEFT)
+listbox1=Listbox(window, width=20, height=20,borderwidth=0,highlightthickness=0)
+listbox1.grid(row=1, column=0,sticky="nsew")
